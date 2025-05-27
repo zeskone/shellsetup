2ca7fcf970394534053c2acd1c9275f08780022d
zeskone <zeskone@outlook.com>
2025-05-26 21:52:31 -0500

    updated setup.sh

--- setup.sh
+++ setup.sh
@@ -1,12 +1,19 @@
 #!/bin/zsh
 
 echo "Installing Fonts..."
-cp ./fonts/* /usr/share/fonts/
+sudo cp -r ./fonts/* /usr/share/fonts/
+echo "Refreshing Font Cache..."
 fc-cache -f
 echo "Are the fonts listed?"
 fc-list | grep -e Caskaydia -e Fira
 echo ""
 
 echo "Installing Starship"
-zsh starship_install.sh
+#zsh starship_install.sh
+sudo apt update && sudo apt install starship -y
+echo "Adding Starship to .zshrc"
 echo 'eval "$(starship init zsh)"' >> ~/.zshrc
+echo "Switching preset to Pastel Powerline"
+starship preset pastel-powerline -o ~/.config/starship.toml
+echo ""
+echo "Done! Close and reopen the shell!"
